9a524154ff174139f282c455cf74850239aa72ec
Praveen Krishnamoorthy <pkrishnamoorthy@excentus.com>
2021-12-26 14:00:00 +0530

    26-12-2021 HashMap Practice problems. LeetCode #290, #771

---
+++ src/hashMap/P01_jewelsAndStones.java
@@ -0,0 +1,134 @@
+package hashMap;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+
+public class P01_jewelsAndStones {
+
+	/*
+	 * PROBLEM STATEMENT 
+	 * 
+	 * LeetCode - 771
+	 * 
+	 * You're given strings jewels representing the types of stones that are jewels, 
+	 * and stones representing the stones you have. 
+	 * Each character in stones is a type of stone you have. 
+	 * You want to know how many of the stones you have are also jewels.
+	 * Letters are case sensitive, so "a" is considered a different type of stone from "A".
+	 * 
+	 * 
+	 * 
+	 */ 
+	 
+	/*
+	 * 1. Did I understand the problem?  -- YES
+	 * 	yes or no 
+	 * 	If no ask the person to provide
+	 * 	with more details with examples 
+	 * 	If yes go to the next step What is the
+	 * 	input(s)? What is the expected output? 
+	 * 
+	 * 	Do I have any constraints to solve the
+	 * 	problem? Do I have all the information to go to the next steps 
+	 * 	How big is your test data set will be? 
+	 * 
+	 * 2. Test data set Minimum 3 data set including positive, negative and edge
+	 * Validate with the interviewer if the data set is fine by his/ her assumptions
+	 * 
+	 * 
+	 * 3. Do I know to solve it? 
+	 * 	Yes- great is there an alternate ? 
+	 * 	No - can I break the problem into sub problems?
+	 * 
+	 * 
+	 * 4. Ask for hint (if you dont know how to solve
+	 * this) 
+	 * 
+	 * 5. Do I know alternate solutions as well Yes- what are those? No- that
+	 * is still fine, proceed to solve by what you know 
+	 * 
+	 * 6. If you know the alternate
+	 * solution find out the O-notations (performance) 
+	 * 
+	 * 
+	 * 7. Then, explain either both are the best (depends on
+	 * the time) 
+	 * 	Approach 1:- start with the worst-> improve (optimize) -> End up
+	 * with the best 
+	 * 	Approach 2: Write down the options and benefits and code the
+	 * best 
+	 * 
+	 * 8. Start always with the Pseudo code 
+	 * 
+	 * 9. Test against different test data
+	 * 
+	 * 10. If it fails then debug to solve it
+	 */
+	
+	@Test
+	public void example1() {
+		//Positive Test Data
+		String jewels = "aA";
+		String stones = "aAAabbbb";
+		jewelsAndStonesHashMap(jewels,stones);
+		
+	}
+	
+	
+
+	@Test
+	public void example2() {
+		//Edge Case Test Data
+		String jewels = "z";
+		String stones = "ZZ";
+		jewelsAndStonesHashMap(jewels,stones);
+	}
+	
+	@Test
+	public void example3() {
+		//Negative Test Data
+		String jewels = "zZa";
+		String stones = "ZZ";
+		jewelsAndStonesHashMap(jewels,stones);
+	}
+	
+	/*
+	 * --- Pseudo Code ---
+	 * 
+	 * 1. Create two pointers left and right
+	 * 2. Point left to jewels and second pointer to stones
+	 * 3. If jewel.charat(left) matches stones.charAt(right), add to hashmap
+	 * 4. While adding to hashmap, check if count already exists. If so, increment by one
+	 * 5. Loop till end of jewels
+	 * 
+	 */	
+	
+	private int jewelsAndStones(String jewels, String stones) {
+		// Brute Force Method
+		int left = 0,right = 0,counter=0;
+		//HashMap<Character,Integer> hMap = new HashMap<>();
+		while (left<jewels.length()) {
+			if (jewels.charAt(left)==stones.charAt(right)) {
+				//hMap.put(jewels.charAt(left), hMap.getOrDefault(jewels.charAt(left), 0)+1);
+				counter++;
+			}
+			right++;
+			if(right==stones.length()) {
+				left++;
+				right=0;
+			}	
+		}
+		System.out.println(counter);
+		return counter;
+	}
+	
+	private int jewelsAndStonesHashMap(String jewels, String stones) {
+		int counter = 0;
+		HashMap<Character,Integer> hMap = new HashMap<>();
+		for(int i = 0;i<jewels.length();i++) hMap.put(jewels.charAt(i), 1);
+		for(int j=0;j<stones.length();j++) if(hMap.containsKey(stones.charAt(j))) counter++;
+		System.out.println(counter);
+		return counter;
+	}
+}
---
+++ src/hashMap/P02_WordPattern.java
@@ -0,0 +1,151 @@
+package hashMap;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class P02_WordPattern {
+
+	/*
+	 * PROBLEM STATEMENT 
+	 * 
+	 * LeetCode No - 290
+	 * 
+	 * Given a pattern and a String s, find if s follows the same pattern.
+	 * 
+	 * Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+	 * 
+	 * Key takeaway -- Values in both strings should be unique
+	 * 
+	 * 
+	 * 
+	 */ 
+	 
+	/*
+	 * 1. Did I understand the problem?  - Yes
+	 * 	yes or no 
+	 * 	If no ask the person to provide
+	 * 	with more details with examples 
+	 * 	If yes go to the next step What is the
+	 * 	input(s)? What is the expected output? 
+	 * 
+	 * 	Do I have any constraints to solve the -- Only lowercase letters in both strings
+	 * 	problem? Do I have all the information to go to the next steps 
+	 * 	How big is your test data set will be? 
+	 * 
+	 * 2. Test data set Minimum 3 data set including positive, negative and edge
+	 * Validate with the interviewer if the data set is fine by his/ her assumptions
+	 * 
+	 * 
+	 * 3. Do I know to solve it? 
+	 * 	Yes- great is there an alternate ? 
+	 * 	No - can I break the problem into sub problems?
+	 * 
+	 * 
+	 * 4. Ask for hint (if you dont know how to solve
+	 * this) 
+	 * 
+	 * 5. Do I know alternate solutions as well Yes- what are those? No- that
+	 * is still fine, proceed to solve by what you know 
+	 * 
+	 * 6. If you know the alternate
+	 * solution find out the O-notations (performance) 
+	 * 
+	 * 
+	 * 7. Then, explain either both are the best (depends on
+	 * the time) 
+	 * 	Approach 1:- start with the worst-> improve (optimize) -> End up
+	 * with the best 
+	 * 	Approach 2: Write down the options and benefits and code the
+	 * best 
+	 * 
+	 * 8. Start always with the Pseudo code 
+	 * 
+	 * 9. Test against different test data
+	 * 
+	 * 10. If it fails then debug to solve it
+	 */
+	
+	@Test
+	public void example1() {
+		//Positive Test Data
+		String pattern = "abba";
+		String s = "dog dog dog dog";
+		wordPatternOptimized(pattern,s);
+		Assert.assertEquals(false, wordPatternOptimized(pattern,s));
+	}
+
+	@Test
+	public void example2() {
+		//Edge Case Test Data
+		String pattern = "abba";
+		String s = "dog cat cat dog";
+		Assert.assertEquals(false, wordPatternOptimized(pattern,s));
+		/*
+		 * String pattern = "aba"; 
+		 * String s = "dog cat cat"; 
+		 * Assert.assertEquals(false,wordPatternOptimized(pattern,s));
+		 */
+	}
+	
+	@Test
+	public void example3() {
+		//Negative Test Data
+		String pattern = "aaaa";
+		String s = "dog cat cat fish";
+		Assert.assertEquals(false, wordPatternOptimized(pattern,s));
+	}
+	
+	/*
+	 * --- Pseudo Code ---
+	 * 
+	 * 1. Create hashmap
+	 * 2. Split the string array by using space
+	 * 2. When a pattern character is not available in hashMap, add it with relevant value from string s
+	 * 3. For each character in pattern, get the respective value from stArray.
+	 * 4. If the value for that character returned from hMap and stArray are not same, return false
+	 */	
+	
+	private boolean wordPattern(String pattern, String s) {
+		HashMap<Character,String> pMap = new HashMap<>();
+		boolean result = true;
+		String[] stArray = s.split(" ");
+		
+		for(int i=0;i<pattern.length();i++) {
+			if(!pMap.containsKey(pattern.charAt(i))) pMap.put(pattern.charAt(i), stArray[i]);
+		}
+		for(int j=0;j<pattern.length();j++) {
+			char ch = pattern.charAt(j);
+			String str = stArray[j];
+			if(!pMap.get(ch).equals(str)) result = false;
+		}
+		System.out.println(result);
+		return result;
+		
+	}
+	// above approach will fail if duplicates in string s
+	// pattern = "abba" s = "dog dog dog dog" -- Understand both should be unique
+	
+	private boolean wordPatternOptimized(String pattern, String s) {
+		boolean result = true;
+		HashMap<Character,String> pMap = new HashMap<>();
+		HashMap<String,Character> sMap = new HashMap<>();
+		String[] stArray = s.split(" ");
+		for(int i=0;i<pattern.length();i++) {
+			if(pattern.length()!=stArray.length) return false;
+			if(!pMap.containsKey(pattern.charAt(i)) && !sMap.containsKey(stArray[i])) {
+				pMap.put(pattern.charAt(i), stArray[i]);
+				sMap.put(stArray[i], pattern.charAt(i));
+			}
+			if(!pMap.containsKey(pattern.charAt(i)) || !sMap.containsKey(stArray[i])) return false;
+			else if(!pMap.get(pattern.charAt(i)).equals(stArray[i]) || !sMap.get(stArray[i]).equals(pattern.charAt(i))) return false;
+		}
+		System.out.println(result);
+		System.out.println(pMap.toString());
+		System.out.println(sMap.toString());
+		return result;
+	}
+}
