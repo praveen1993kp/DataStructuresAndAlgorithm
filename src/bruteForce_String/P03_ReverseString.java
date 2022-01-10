ffc6fcc0b4e06b827f98ef2ef87770c0ab55cdfe
Praveen Krishnamoorthy <pkrishnamoorthy@excentus.com>
2022-01-10 13:33:17 +0530

    Old Practice learning Problems

---
+++ src/bruteForce_String/P03_ReverseString.java
@@ -0,0 +1,118 @@
+package bruteForce_String;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class P03_ReverseString {
+
+	/*
+	 * PROBLEM STATEMENT 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */ 
+	 
+	/*
+	 * 1. Did I understand the problem? 
+	 * 	yes or no 
+	 * 	If no ask the person to provide
+	 * 	with more details with examples 
+	 * 	If yes go to the next step What is the
+	 * 	input(s)? What is the expected output? 
+	 * 
+	 * 	Do I have any constraints to solve the
+	 * 	problem? Do I have all the information to go to the next steps 
+	 * 	How big is your test data set will be? 
+	 * 
+	 * 2. Test data set Minimum 3 data set including positive, negative and edge
+	 * Validate with the interviewer if the data set is fine by his/ her assumptions
+	 * 
+	 * 
+	 * 3. Do I know to solve it? 
+	 * 	Yes- great is there an alternate ? 
+	 * 	No - can I break the problem into sub problems?
+	 * 
+	 * 
+	 * 4. Ask for hint (if you dont know how to solve
+	 * this) 
+	 * 
+	 * 5. Do I know alternate solutions as well Yes- what are those? No- that
+	 * is still fine, proceed to solve by what you know 
+	 * 
+	 * 6. If you know the alternate
+	 * solution find out the O-notations (performance) 
+	 * 
+	 * 
+	 * 7. Then, explain either both are the best (depends on
+	 * the time) 
+	 * 	Approach 1:- start with the worst-> improve (optimize) -> End up
+	 * with the best 
+	 * 	Approach 2: Write down the options and benefits and code the
+	 * best 
+	 * 
+	 * 8. Start always with the Pseudo code 
+	 * 
+	 * 9. Test against different test data
+	 * 
+	 * 10. If it fails then debug to solve it
+	 */
+	
+	@Test
+	public void example1() {
+		//Positive Test Data
+		String str = "This is the String";
+		String output = "String eht is sihT";
+		Assert.assertEquals(output, reverseStringAndSwap(str));
+	}
+	
+
+
+	@Test
+	public void example2() {
+		//Edge Case Test Data
+		String str = "";
+		String output = "";
+		Assert.assertEquals(output, reverseStringAndSwap(str));
+	}
+	
+	@Test
+	public void example3() {
+		//Negative Test Data
+		String str = "This This";
+		String output = "This sihT";
+		Assert.assertEquals(output, reverseStringAndSwap(str));
+	}
+	
+	/*
+	 * --- Pseudo Code ---
+	 * 
+	 */	
+	
+	private String reverseStringAndSwap(String str) {
+		String[] strArray = str.split(" ");
+		int left = 0,right = strArray.length-1;
+		
+		while(left<right) {
+			String temp = strArray[left];
+			strArray[left++] = strArray[right];
+			strArray[right--] = temp;
+		}
+		
+		String output = "";
+		for(int i=0;i<strArray.length;i++) {
+			int index = strArray[i].length()-1;
+			if(i!=0) output += " ";
+			if(i%2==1) {
+				while(index>=0) output += strArray[i].charAt(index--);
+				strArray[i] = output;
+			} else output += strArray[i];
+		}
+		
+		return output;
+	}
+}
---
+++ src/hashMap/P05_FindMaxNumberAndItsOccurrence.java
@@ -0,0 +1,115 @@
+package hashMap;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import org.junit.Test;
+
+public class P05_FindMaxNumberAndItsOccurrence {
+
+	/*
+	 * PROBLEM STATEMENT 
+	 * 
+	 * Given an integer array nums, find the max frequency number and its occurrence.
+	 * 
+	 * 
+	 * 
+	 */ 
+	 
+	/*
+	 * 1. Did I understand the problem? 
+	 * 	yes or no 
+	 * 	If no ask the person to provide
+	 * 	with more details with examples 
+	 * 	If yes go to the next step What is the
+	 * 	input(s)? What is the expected output? 
+	 * 
+	 * 	Do I have any constraints to solve the
+	 * 	problem? Do I have all the information to go to the next steps 
+	 * 	How big is your test data set will be? 
+	 * 
+	 * 2. Test data set Minimum 3 data set including positive, negative and edge
+	 * Validate with the interviewer if the data set is fine by his/ her assumptions
+	 * 
+	 * 
+	 * 3. Do I know to solve it? 
+	 * 	Yes- great is there an alternate ? 
+	 * 	No - can I break the problem into sub problems?
+	 * 
+	 * 
+	 * 4. Ask for hint (if you dont know how to solve
+	 * this) 
+	 * 
+	 * 5. Do I know alternate solutions as well Yes- what are those? No- that
+	 * is still fine, proceed to solve by what you know 
+	 * 
+	 * 6. If you know the alternate
+	 * solution find out the O-notations (performance) 
+	 * 
+	 * 
+	 * 7. Then, explain either both are the best (depends on
+	 * the time) 
+	 * 	Approach 1:- start with the worst-> improve (optimize) -> End up
+	 * with the best 
+	 * 	Approach 2: Write down the options and benefits and code the
+	 * best 
+	 * 
+	 * 8. Start always with the Pseudo code 
+	 * 
+	 * 9. Test against different test data
+	 * 
+	 * 10. If it fails then debug to solve it
+	 */
+	
+	@Test
+	public void example1() {
+		//Positive Test Data
+		int[] nums = {1,2,2,3,4,5,4,2};
+		findTheNumber(nums);
+		
+	}
+	
+	
+
+	@Test
+	public void example2() {
+		//Edge Case Test Data
+		int[] nums = {1,2,3,4,5,4,2};
+		findTheNumber(nums);
+	}
+	
+	@Test
+	public void example3() {
+		//Negative Test Data
+		int[] nums = {1,2,3,4,5};
+		findTheNumber(nums);
+	}
+	
+	/*
+	 * --- Pseudo Code ---
+	 * 
+	 */	
+	
+	private void findTheNumber(int[] nums) {
+		HashMap<Integer,Integer> hMap = new HashMap<>();
+		int maxNum = 0, maxFrequency = Integer.MIN_VALUE;
+		
+		for(int currentElement=0;currentElement<nums.length;currentElement++) {
+			hMap.put(nums[currentElement], hMap.getOrDefault(nums[currentElement],0)+1);
+			if(hMap.get(nums[currentElement])>maxFrequency) {
+				maxFrequency = hMap.get(nums[currentElement]);
+				maxNum = nums[currentElement];
+			}
+		}
+		System.out.println(maxNum);
+		System.out.println(maxFrequency);  // return only one element
+		System.out.println("******");
+		//return all the numbers with max Frequency
+		
+		for (Entry<Integer,Integer> entry : hMap.entrySet()) {
+			if(entry.getValue() == maxFrequency){
+				System.out.println("Element : " + entry.getKey() + " -- NoOfOccurrences : " + entry.getValue());
+			}
+		}
+	}
+}
