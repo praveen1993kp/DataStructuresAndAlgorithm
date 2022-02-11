d42c247d0eff92d0f390a4516191768f8e264b5d
Praveen Krishnamoorthy <praveen1993kp@gmail.com>
2022-02-11 20:04:40 +0530

    Leetcode #560 - Solution added - No clarity on pseudo code

---
+++ src/slidingWindow_Arrays/P09_SubarraySumEqualsK.java
@@ -0,0 +1,107 @@
+package slidingWindow_Arrays;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+
+public class P09_SubarraySumEqualsK {
+
+	/*
+	 * PROBLEM STATEMENT 
+	 * 
+	 * Leetcode - 560
+	 * 
+	 * Given an array of integers nums and an integer k, return the total number of continuous
+	 * subarrays whose sum equals to k.
+	 * 
+	 * 
+	 * 
+	 */ 
+	 
+	/*
+	 * 1. Did I understand the problem? 
+	 * 	yes or no 
+	 * 	If no ask the person to provide
+	 * 	with more details with examples 
+	 * 	If yes go to the next step What is the
+	 * 	input(s)? What is the expected output? 
+	 * 
+	 * 	Do I have any constraints to solve the
+	 * 	problem? Do I have all the information to go to the next steps 
+	 * 	How big is your test data set will be? 
+	 * 
+	 * 2. Test data set Minimum 3 data set including positive, negative and edge
+	 * Validate with the interviewer if the data set is fine by his/ her assumptions
+	 * 
+	 * 
+	 * 3. Do I know to solve it? 
+	 * 	Yes- great is there an alternate ? 
+	 * 	No - can I break the problem into sub problems?
+	 * 
+	 * 
+	 * 4. Ask for hint (if you dont know how to solve
+	 * this) 
+	 * 
+	 * 5. Do I know alternate solutions as well Yes- what are those? No- that
+	 * is still fine, proceed to solve by what you know 
+	 * 
+	 * 6. If you know the alternate
+	 * solution find out the O-notations (performance) 
+	 * 
+	 * 
+	 * 7. Then, explain either both are the best (depends on
+	 * the time) 
+	 * 	Approach 1:- start with the worst-> improve (optimize) -> End up
+	 * with the best 
+	 * 	Approach 2: Write down the options and benefits and code the
+	 * best 
+	 * 
+	 * 8. Start always with the Pseudo code 
+	 * 
+	 * 9. Test against different test data
+	 * 
+	 * 10. If it fails then debug to solve it
+	 */
+	
+	@Test
+	public void example1() {
+		//Positive Test Data
+		int[] nums = {1,2,3,1,4,5,1,-1};
+		int k = 5;
+		sumEqualsK(nums,k);
+	}
+	
+	
+
+	@Test
+	public void example2() {
+		//Edge Case Test Data
+		int[] nums = {-1,-1,1};
+		int k = 0;
+		sumEqualsK(nums,k);
+	}
+	
+	@Test
+	public void example3() {
+		//Negative Test Data
+		
+	}
+	
+	/*
+	 * --- Pseudo Code ---
+	 * 
+	 */	
+	
+	private int sumEqualsK(int[] nums, int k) {
+		int count = 0, sum = 0;
+        HashMap <Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+	}
+}
