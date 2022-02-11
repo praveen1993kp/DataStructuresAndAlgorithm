12c58a87878f4ea93b2df6b3dda1b599727ce662
Praveen Krishnamoorthy <praveen1993kp@gmail.com>
2022-02-11 13:56:59 +0530

    Leetcode #2154 - Keep Multiplying Found Values By Two - Solution Added

---
+++ src/twoPointer_Arrays/P09_KeepMultiplyingFoundValuesByTwo.java
@@ -0,0 +1,109 @@
+package twoPointer_Arrays;
+
+import org.junit.Test;
+
+public class P09_KeepMultiplyingFoundValuesByTwo {
+
+	/*
+	 * PROBLEM STATEMENT 
+	 * 
+	 * Leetcode - 2154
+	 * 
+	 * 
+	 * 
+	 */ 
+	 
+	/*
+	 * 1. Did I understand the problem? 
+	 * 	yes or no 
+	 * 	If no ask the person to provide
+	 * 	with more details with examples 
+	 * 	If yes go to the next step What is the
+	 * 	input(s)? What is the expected output? 
+	 * 
+	 * 	Do I have any constraints to solve the
+	 * 	problem? Do I have all the information to go to the next steps 
+	 * 	How big is your test data set will be? 
+	 * 
+	 * 2. Test data set Minimum 3 data set including positive, negative and edge
+	 * Validate with the interviewer if the data set is fine by his/ her assumptions
+	 * 
+	 * 
+	 * 3. Do I know to solve it? 
+	 * 	Yes- great is there an alternate ? 
+	 * 	No - can I break the problem into sub problems?
+	 * 
+	 * 
+	 * 4. Ask for hint (if you dont know how to solve
+	 * this) 
+	 * 
+	 * 5. Do I know alternate solutions as well Yes- what are those? No- that
+	 * is still fine, proceed to solve by what you know 
+	 * 
+	 * 6. If you know the alternate
+	 * solution find out the O-notations (performance) 
+	 * 
+	 * 
+	 * 7. Then, explain either both are the best (depends on
+	 * the time) 
+	 * 	Approach 1:- start with the worst-> improve (optimize) -> End up
+	 * with the best 
+	 * 	Approach 2: Write down the options and benefits and code the
+	 * best 
+	 * 
+	 * 8. Start always with the Pseudo code 
+	 * 
+	 * 9. Test against different test data
+	 * 
+	 * 10. If it fails then debug to solve it
+	 */
+	
+	@Test
+	public void example1() {
+		//Positive Test Data
+		int[] nums = {5,3,6,1,12};
+		int original = 3;
+		findOriginal(nums,original);
+	}
+	
+	@Test
+	public void example2() {
+		//Edge Case Test Data
+		int[] nums = {32,16,8,4,2,1};
+		int original = 1;
+		findOriginal(nums,original);
+	}
+	
+	@Test
+	public void example3() {
+		//Negative Test Data
+		int[] nums = {32,16,8,4,2,1};
+		int original = 3;
+		findOriginal(nums,original);
+	}
+	
+	
+
+	/*
+	 * --- Pseudo Code ---
+	 * 
+	 * 1. Create variable index = 0
+	 * 2. Traverse through the given array
+	 * 3. If given array contains original, multiply the original by 2 and reset index to 0
+	 * 4. Else increment the index
+	 * 5. Continue till index crosses the array length
+	 * 
+	 */	
+	
+	private int findOriginal(int[] nums, int original) {
+		int index = 0;
+		while(index<nums.length) {
+			if(nums[index]==original) {
+				original *= 2;
+				index = 0;
+			} else index++;
+		}
+		System.out.println(original);
+		return original;
+	}
+}
