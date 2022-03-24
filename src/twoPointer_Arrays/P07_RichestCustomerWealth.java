716abcb19d65f369d7c6ac4189dcebe201f690b1
Praveen Krishnamoorthy <praveen1993kp@gmail.com>
2022-03-24 11:53:44 +0530

    Leetcode #1672 - Richest customer Wealth - Problem Statement Added

--- src/twoPointer_Arrays/P07_RichestCustomerWealth.java
+++ src/twoPointer_Arrays/P07_RichestCustomerWealth.java
@@ -9,7 +9,12 @@ public class P07_RichestCustomerWealth {
 	 * 
 	 * Leetcode - 1672
 	 * 
+	 * You are given an m x n integer grid accounts where accounts[i][j] 
+	 * is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. 
+	 * Return the wealth that the richest customer has.
 	 * 
+	 * A customer's wealth is the amount of money they have in all their bank accounts. 
+	 * The richest customer is the customer that has the maximum wealth.
 	 * 
 	 */ 
 	 
@@ -109,7 +114,7 @@ public class P07_RichestCustomerWealth {
 			}
 			max = Math.max(sum, max);						
 		}
-		//System.out.println(max);
+		//System.out.println(max); 
 		return max; 
 	}
 }
